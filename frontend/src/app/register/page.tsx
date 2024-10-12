cb6e23aec9d6aa8066256c39e0e9e2bf1d4ef529
danmandel <danmandel@users.noreply.github.com>
2024-10-12 01:33:55 -0400

    linting

--- frontend/src/app/register/page.tsx
+++ frontend/src/app/register/page.tsx
@@ -49,45 +49,45 @@ const RegisterPage = () => {
   };
 
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center bg-gray-100">
-      <div className="w-96 rounded bg-white p-8 shadow-md">
-        <h1 className="mb-8 text-4xl font-bold text-black">Create an account</h1>
+    <div className='flex min-h-screen flex-col items-center justify-center bg-gray-100'>
+      <div className='w-96 rounded bg-white p-8 shadow-md'>
+        <h1 className='mb-8 text-4xl font-bold text-black'>Create an account</h1>
         <form onSubmit={handleRegister}>
-          <div className="mb-4">
-            <label htmlFor="email" className="block text-gray-700">
+          <div className='mb-4'>
+            <label htmlFor='email' className='block text-gray-700'>
               Email
             </label>
             <input
-              type="email"
-              id="email"
+              type='email'
+              id='email'
               value={email}
               onChange={(e) => setEmail(e.target.value)}
-              className="w-full rounded border px-3 py-2 text-black"
+              className='w-full rounded border px-3 py-2 text-black'
               required
             />
           </div>
-          <div className="mb-4">
-            <label htmlFor="password" className="block text-gray-700">
+          <div className='mb-4'>
+            <label htmlFor='password' className='block text-gray-700'>
               Password
             </label>
             <input
-              type="password"
-              id="password"
+              type='password'
+              id='password'
               value={password}
               onChange={(e) => setPassword(e.target.value)}
-              className="w-full rounded border px-3 py-2 text-black"
+              className='w-full rounded border px-3 py-2 text-black'
               required
             />
           </div>
-          {error && <p className="mb-4 text-red-500">{error}</p>}
-          <button type="submit" className="w-full rounded bg-blue-500 py-2 text-white">
+          {error && <p className='mb-4 text-red-500'>{error}</p>}
+          <button type='submit' className='w-full rounded bg-blue-500 py-2 text-white'>
             Register
           </button>
         </form>
-        <div className="my-4 flex items-center text-center text-gray-500">
-          <hr className="my-2 grow" />
-          <span className="mx-2">Or register with</span>
-          <hr className="my-2 grow" />
+        <div className='my-4 flex items-center text-center text-gray-500'>
+          <hr className='my-2 grow' />
+          <span className='mx-2'>Or register with</span>
+          <hr className='my-2 grow' />
         </div>
 
         <GoogleButton onClick={handleGoogleSignIn} />
